4f371a7b7937e369c282cce8978ea568a09ca32c
Juan rojas <71771380+Juan-rojas75@users.noreply.github.com>
2025-08-21 17:04:33 -0500

    bug texto invertido

--- parametrization-interfaces/src/app/home/template/[slug]/components/ConfigModal/const/type.const.ts
+++ parametrization-interfaces/src/app/home/template/[slug]/components/ConfigModal/const/type.const.ts
@@ -3,5 +3,5 @@ export const typesOptions = [
     { id: 2, name: "Número", value: "number" },
     { id: 3, name: "Fecha", value: "date" },
     { id: 4, name: "Auto-numero", value: "auto-number" },
-    { id: 5, name: "Texto invertido", value: "string-invert" }
+    { id: 5, name: "Texto invertido", value: "string-inverted" }
   ]
